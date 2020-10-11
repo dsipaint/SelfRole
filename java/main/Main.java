b78e3a33cc31284635c4bfba5c3b37b24f0a9ffe
dsipaint <57596940+dsipaint@users.noreply.github.com>
2020-10-11 17:35:11 +0100

    Add files via upload

---
+++ java/main/Main.java
@@ -0,0 +1,64 @@
+package main;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.security.auth.login.LoginException;
+
+import org.json.simple.DeserializationException;
+
+import listeners.AdminCommandListener;
+import listeners.RoleDeleteListener;
+import listeners.RoleListener;
+import listeners.StopListener;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+public class Main
+{
+  static JDA jda;
+  public static final String PREFIX = "^";
+  public static HashMap<String, ArrayList<String>> selfroles;
+  
+	public static void main(String[] args)
+	{
+		try
+		{
+		  jda = JDABuilder.createDefault("")
+				  .enableIntents(GatewayIntent.GUILD_MEMBERS)
+				  .build();
+		}
+		catch (LoginException e)
+		{
+		  e.printStackTrace();
+		} 
+		
+		try
+		{
+		  jda.awaitReady();
+		}
+		catch (InterruptedException e)
+		{
+		  
+		  e.printStackTrace();
+		}
+		
+		jda.getGuildById("565623426501443584").loadMembers();
+		
+		try
+		{
+			selfroles = Data.readSelfRoles();
+		}
+		catch (IOException | DeserializationException e)
+		{
+			e.printStackTrace();
+		}
+		
+		jda.addEventListener(new RoleListener());
+		jda.addEventListener(new AdminCommandListener());
+		jda.addEventListener(new StopListener());
+		jda.addEventListener(new RoleDeleteListener());
+	}
+}
